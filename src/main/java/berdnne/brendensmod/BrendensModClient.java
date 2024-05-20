ddb875861f0250f8789e62d6b5e632859ef0711e
berdnne <90113754+berdnne@users.noreply.github.com>
2024-05-19 23:26:12 -0500

    Initial commit

---
+++ src/main/java/berdnne/brendensmod/BrendensModClient.java
@@ -0,0 +1,11 @@
+package berdnne.brendensmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BrendensModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/berdnne/brendensmod/BrendensModDataGenerator.java
@@ -0,0 +1,11 @@
+package berdnne.brendensmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BrendensModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
